b7decc29a2db7248b55159ad8d3f3f2e6e70a5ba
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-03 01:56:35 +0100

    Add move_to_end

--- lua/grapple-utils.lua
+++ lua/grapple-utils.lua
@@ -2,8 +2,14 @@ local M = {}
 
 function M.setup() end
 
-function M.test()
-	print("Hello, World!")
+function M.move_to_end()
+	local grapple = require("grapple")
+	local current_path = vim.api.nvim_buf_get_name(0)
+	local opts = { path = current_path }
+	if grapple.exists(opts) then
+		grapple.untag(opts)
+		grapple.tag(opts)
+	end
 end
 
 return M
